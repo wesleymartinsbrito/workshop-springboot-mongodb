1890aec6ee988cadc2ba13dd47de8d4585d7044d
Wesley Martins <wesleymartins.dev@outlook.com>
2023-09-25 20:14:48 -0300

    First commit

---
+++ src/main/java/com/wesleymartins/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.wesleymartins.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesleymartins/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleymartins.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

35f68434cdcca0585c4c599e5074e3ca55a70ebb
Wesley Martins <wesleymartins.dev@outlook.com>
2023-09-27 19:30:47 -0300

    user update with put

--- src/main/java/com/wesleymartins/workshopmongo/repositories/UserRepository.java
+++ src/main/java/com/wesleymartins/workshopmongo/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.wesleymartins.workshopmongo.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.wesleymartins.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+	User save(Optional<User> newObj);
 	
 }

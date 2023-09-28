0fa1de4ed0b6f9421c7575eb5c87d17c2afae561
Wesley Martins <wesleymartins.dev@outlook.com>
2023-09-28 19:15:26 -0300

    Endpoint to return user posts

--- src/main/java/com/wesleymartins/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wesleymartins/workshopmongo/resources/UserResource.java
@@ -8,13 +8,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.wesleymartins.workshopmongo.domain.Post;
 import com.wesleymartins.workshopmongo.domain.User;
 import com.wesleymartins.workshopmongo.dto.UserDTO;
 import com.wesleymartins.workshopmongo.services.UserService;
@@ -52,12 +52,18 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
-	@RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
 		User obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		Optional<User> obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.get().getPosts());
 	}
 }

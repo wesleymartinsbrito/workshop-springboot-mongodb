7884a67ced98a07a1b64f7a198d909c625f4fedb
Wesley Martins <wesleymartins.dev@outlook.com>
2023-10-03 17:59:58 -0300

    Simple search with Query

--- src/main/java/com/wesleymartins/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/wesleymartins/workshopmongo/repositories/PostRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.wesleymartins.workshopmongo.domain.Post;
@@ -13,6 +14,9 @@ import com.wesleymartins.workshopmongo.domain.User;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	User save(Optional<User> newObj);
+	
+	@Query("{ 'title': {$regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 		
